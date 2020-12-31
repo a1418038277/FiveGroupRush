a1154a5a87a09b793cec9b64cd8e4108d602ad65
z2033864981 <2033864981@qq.com>
2020-12-31 11:44:31 +0800

    更新注释

--- app/src/main/java/firsttest/test/fivegroupproject/MainActivity.java
+++ app/src/main/java/firsttest/test/fivegroupproject/MainActivity.java
@@ -3,11 +3,31 @@ package firsttest.test.fivegroupproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/**     
+  * 
+  * @ProjectName:    
+  * @Package:        firsttest.test.fivegroupproject
+  * @ClassName:      MainActivity
+  * @Description:     java类作用描述 
+  * @Author:         zfc
+  * @CreateDate:     2020/12/31 11:42
+  * @UpdateUser:     zfc
+  * @UpdateDate:     2020/12/31 11:42
+  * @UpdateRemark:   更新说明：
+  * @Version:        1.0
+ */
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /**
+         * @method  onCreate
+         * @description 描述一下方法的作用
+         * @date: 2020/12/31 11:43
+         * @author: zfc
+         * @param 
+         * @return void
+         */
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }

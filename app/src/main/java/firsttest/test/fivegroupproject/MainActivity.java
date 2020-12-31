decfb38f3b6285f3485fdce872e84b051d355b07
d <c2545358712@163.com>
2020-12-31 10:31:28 +0800

    Viewpager导航页

--- app/src/main/java/firsttest/test/fivegroupproject/MainActivity.java
+++ app/src/main/java/firsttest/test/fivegroupproject/MainActivity.java
@@ -1,14 +1,94 @@
 package firsttest.test.fivegroupproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import firsttest.test.fivegroupproject.base.BaseAcitvity;
+import firsttest.test.fivegroupproject.interfaces.IBasePresenter;
+
+public class MainActivity extends BaseAcitvity {
+
+    @BindView(R.id.viewpager)
+    ViewPager viewpager;
+    private ArrayList<Integer> integers;
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected IBasePresenter createPrenter() {
+        return null;
+    }
+
+    @Override
+    protected void initView() {
+        integers = new ArrayList<>();
+        integers.add(R.drawable.ic_vp_one);
+        integers.add(R.drawable.ic_vp_two);
+        integers.add(R.drawable.ic_vp_three);
+        viewpager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return integers.size();
+            }
+
+            @NonNull
+            @Override
+            public Object instantiateItem(@NonNull ViewGroup container, int position) {
+                View inflate = LayoutInflater.from(MainActivity.this).inflate(R.layout.vp_item, null);
+                ImageView img = inflate.findViewById(R.id.img);
+                img.setImageResource(integers.get(position));
+                View btnJump = inflate.findViewById(R.id.btn_jump);
+                if (position == integers.size() - 1) {
+                    btnJump.setVisibility(View.VISIBLE);
+                    btnJump.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                            startActivity(intent);
+                        }
+                    });
+
+                }
+                container.addView(inflate);
+                return inflate;
+            }
+
+            @Override
+            public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+                return view==object;
+            }
+
+            @Override
+            public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+                container.removeView((View) object);
+            }
+        });
+    }
+
+    @Override
+    protected void initData() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }

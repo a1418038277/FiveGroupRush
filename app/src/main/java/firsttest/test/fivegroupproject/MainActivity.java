49e3dcf20d3e8377c126578316da9e9388cc541f
a1418038277 <a1418038277@163.com>
2020-12-31 11:59:37 +0800

    vp和tab更新

--- app/src/main/java/firsttest/test/fivegroupproject/MainActivity.java
+++ app/src/main/java/firsttest/test/fivegroupproject/MainActivity.java
@@ -1,14 +1,26 @@
 package firsttest.test.fivegroupproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.vp)
+    ViewPager vp;
+    @BindView(R.id.tab)
+    TabLayout tab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
